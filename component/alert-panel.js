afcdffdcd079ba26a21de75a7998f23731015768
Abigail Watson <awatson1978@gmail.com>
2014-12-06 13:58:33 -0500

    initial commit

---
+++ component/alert-panel.js
@@ -0,0 +1,56 @@
+//======================================================
+// defaults
+
+Session.setDefault('alertMessage', false);
+
+
+//======================================================
+// alert panel
+
+Template.alertPanel.helpers({
+  displayAlert: function(){
+    if(Session.get('alertMessage')){
+      return true;
+    }else{
+      return false;
+    }
+  },
+  getAlertMessage: function(){
+    if(Session.get('alertMessage')){
+      return Session.get('alertMessage');
+    }
+  }
+});
+Template.alertPanel.events({
+  "click #alertPanel":function(){
+    Session.set('alertMessage', false);
+  }
+});
+
+
+
+//======================================================
+// this is only for diagnostics and testing
+
+Template.toggleAlert.events({
+  "click #toggleAlertLink": function(event, template){
+    if(Session.get('alertMessage')){
+      Session.set('alertMessage', false);
+    }else{
+      Session.set('alertMessage', 'Toggle!');
+    }
+  }
+});
+
+
+//======================================================
+// API Object
+
+AlertPanel = {
+  announce: function(message){
+    Session.set('alertMessage', message);
+  },
+  close: function(){
+    Session.set('alertMessage', false);
+  }
+}
---
+++ package.js
@@ -0,0 +1,30 @@
+Package.describe({
+  name: 'clinical:alert-panel',
+  summary: 'Top-anchored alert panel designed to work with header navbars.',
+  version: '1.0.0',
+  git: 'http://github.com/awatson1978/clinical-alert-panel.git'
+});
+
+Package.onUse(function(api) {
+  api.versionsFrom('1.0');
+
+  api.use('less@1.0.11', 'client');
+  api.use('templating@1.0.9', 'client');
+  api.use('session@1.0.4', 'client');
+
+  api.addFiles('component/alert-panel.less', 'client');
+  api.addFiles('component/alert-panel.html', 'client');
+  api.addFiles('component/alert-panel.js', 'client');
+
+  api.export('AlertPanel', 'client');
+});
+
+Package.onTest(function(api) {
+  api.use('tinytest');
+  api.use('less');
+  api.use('templating');
+  api.use('session');
+  api.use('clinical:alert-panel');
+
+  api.addFiles('alert-panel-tests.js');
+});
